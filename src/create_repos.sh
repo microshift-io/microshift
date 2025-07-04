2e49649f5e0636d1840c4ecc4aee7be7823fdfe1
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-07-04 13:38:27 +0530

    create_repo: Don't hard code the major version of ocp version

--- src/create_repos.sh
+++ src/create_repos.sh
@@ -2,6 +2,7 @@
 
 
 repo_path=$1
+ocp_major_version=$(dnf --quiet --disablerepo="*" --repofrompath=ushift,file://"${repo_path}" --enablerepo=ushift  repoquery --quiet --qf "%{VERSION}" microshift | cut -d. -f1,2)
 
 USHIFT_LOCAL_REPO_FILE=/etc/yum.repos.d/microshift-local.repo
 OCP_MIRROR_REPO_FILE=/etc/yum.repos.d/openshift-mirror-beta.repo
@@ -18,7 +19,7 @@ EOF
     cat > "${OCP_MIRROR_REPO_FILE}" <<EOF
 [openshift-mirror-beta]
 name=OpenShift Mirror Beta Repository
-baseurl=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/dependencies/rpms/4.19-el9-beta/
+baseurl=https://mirror.openshift.com/pub/openshift-v4/$(uname -m)/dependencies/rpms/${ocp_major_version}-el9-beta/
 enabled=1
 gpgcheck=0
 skip_if_unavailable=0
