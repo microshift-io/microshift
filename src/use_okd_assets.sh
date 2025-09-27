9b80113326b40cb7c9e1ab103fe6d2300546d865
Gregory Giguashvili <ggiguash@redhat.com>
2025-09-27 13:15:59 +0300

    Minor style adjustments in config scripts

--- src/use_okd_assets.sh
+++ src/use_okd_assets.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
+set -euo pipefail
 
-set -eo pipefail
-
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MICROSHIFT_ROOT="${SCRIPTDIR}/../microshift"
-
+MICROSHIFT_ROOT="/home/microshift/microshift"
 declare -A UNAME_TO_GOARCH_MAP=( ["x86_64"]="amd64" ["aarch64"]="arm64" )
-
 
 verify(){
     local -r okd_url=$1
     local -r okd_releaseTag=$2
 
-    #stdout=$(oc adm release info "${okd_url}:${okd_releaseTag}" 2>&1)
     if ! stdout=$(oc adm release info "${okd_url}:${okd_releaseTag}" 2>&1)  ; then
         echo -e "error verifying okd release (URL: ${okd_url} , TAG: ${okd_releaseTag}) \nERROR: ${stdout}"
         exit 1
@@ -25,13 +20,11 @@ replace_assets(){
     local -r arch=$(uname -m)
     local -r temp_release_json=$(mktemp "/tmp/release-${arch}.XXXXX.json")
 
-    oc adm release info --image-for="${op}" "${okd_url}:${okd_releaseTag}"
-
     # replace Microshift images with upstream (from OKD release)
-    for op in $(jq -e -r  '.images | keys []' "${MICROSHIFT_ROOT}/assets/release/release-${arch}.json") 
+    for op in $(jq -e -r  '.images | keys []' "${MICROSHIFT_ROOT}/assets/release/release-${arch}.json")
     do
         local image
-        image=$(oc adm release info --image-for="${op}" "${okd_url}:${okd_releaseTag}" || true) 
+        image=$(oc adm release info --image-for="${op}" "${okd_url}:${okd_releaseTag}" || true)
         if [ -n "${image}" ] ; then
             echo "${op} ${image}"
             jq --arg a "${op}" --arg b "${image}"  '.images[$a] = $b' "${MICROSHIFT_ROOT}/assets/release/release-${arch}.json" >"${temp_release_json}"
@@ -39,9 +32,9 @@ replace_assets(){
         fi
     done
 
-    pod_image=$(oc adm release info --image-for=pod "${okd_url}:${okd_releaseTag}" || true) 
+    pod_image=$(oc adm release info --image-for=pod "${okd_url}:${okd_releaseTag}" || true)
     # update the infra pods for crio
-    sed -i 's,pause_image .*,pause_image = '"\"${pod_image}\""',' "packaging/crio.conf.d/10-microshift_${UNAME_TO_GOARCH_MAP[${arch}]}.conf"
+    sed -i 's,pause_image .*,pause_image = '"\"${pod_image}\""',' "${MICROSHIFT_ROOT}/packaging/crio.conf.d/10-microshift_${UNAME_TO_GOARCH_MAP[${arch}]}.conf"
 
     # kube proxy is required for kindnet
     kube_proxy_okd_image_with_hash=$(oc adm release info --image-for="kube-proxy" "${okd_url}:${okd_releaseTag}")
@@ -135,7 +128,7 @@ EOF
 
 usage() {
     echo "Usage:"
-    echo "$(basename "$0") --verify OKD_URL RELEASE_TAG         verify upstream release"
+    echo "$(basename "$0") --verify  OKD_URL RELEASE_TAG         verify upstream release"
     echo "$(basename "$0") --replace OKD_URL RELEASE_TAG         replace microshift assets with upstream images"
     exit 1
 }
@@ -148,7 +141,7 @@ if [ $# -eq 3 ] ; then
         ;;
     --verify)
         verify "$2" "$3"
-        ;;        
+        ;;
     *)
         usage
         ;;
@@ -156,5 +149,3 @@ if [ $# -eq 3 ] ; then
 else
     usage
 fi
-
-
