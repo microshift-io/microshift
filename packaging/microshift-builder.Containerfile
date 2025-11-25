b118c9cbafd7b7a312432a406904423d921066f1
Patryk Matuszak <pmatusza@redhat.com>
2025-11-25 18:42:12 +0100

    Script to build RPMs with deterministic version

--- packaging/microshift-builder.Containerfile
+++ packaging/microshift-builder.Containerfile
@@ -13,6 +13,7 @@ ARG BUILDER_RPM_REPO_PATH=${HOME}/microshift/_output/rpmbuild/RPMS
 ARG USHIFT_PREBUILD_SCRIPT=/tmp/prebuild.sh
 ARG USHIFT_POSTBUILD_SCRIPT=/tmp/postbuild.sh
 ARG USHIFT_MODIFY_SPEC_SCRIPT=/tmp/modify-spec.py
+ARG USHIFT_BUILDRPMS_SCRIPT=/tmp/build-rpms.sh
 
 # Verify mandatory build arguments
 RUN if [ -z "${OKD_VERSION_TAG}" ] ; then \
@@ -46,6 +47,7 @@ WORKDIR ${HOME}/microshift/
 COPY --chmod=755 ./src/image/prebuild.sh ${USHIFT_PREBUILD_SCRIPT}
 RUN "${USHIFT_PREBUILD_SCRIPT}" --replace "${OKD_RELEASE_IMAGE}" "${OKD_VERSION_TAG}"
 
+COPY --chmod=755 ./src/image/build-rpms.sh ${USHIFT_BUILDRPMS_SCRIPT}
 # Modify the microshift.spec to remove packages not yet supported by the upstream.
 COPY --chmod=755 ./src/image/modify-spec.py ${USHIFT_MODIFY_SPEC_SCRIPT}
 # Disable the RPM and SRPM checks in the make-rpm.sh script.
@@ -54,7 +56,7 @@ RUN sed -i -e 's,CHECK_RPMS="y",,g' -e 's,CHECK_SRPMS="y",,g' ./packaging/rpm/ma
 
 # Building all MicroShift downstream RPMs and SRPMs
 # hadolint ignore=DL3059
-RUN MICROSHIFT_VARIANT="community" make -C "${HOME}/microshift" rpm srpm
+RUN "${USHIFT_BUILDRPMS_SCRIPT}" rpm srpm
 
 # Building Kindnet upstream RPM
 COPY --chown=${USER}:${USER} ./src/kindnet/kindnet.spec "${HOME}/microshift/packaging/rpm/microshift.spec"
@@ -63,7 +65,7 @@ COPY --chown=${USER}:${USER} ./src/kindnet/dropins/ "${HOME}/microshift/packagin
 COPY --chown=${USER}:${USER} ./src/kindnet/crio.conf.d/ "${HOME}/microshift/packaging/crio.conf.d/"
 # Prepare and build Kindnet upstream RPM
 RUN "${USHIFT_PREBUILD_SCRIPT}" --replace-kindnet "${OKD_RELEASE_IMAGE}" "${OKD_VERSION_TAG}" && \
-    MICROSHIFT_VARIANT="community" make -C "${HOME}/microshift" rpm
+    "${USHIFT_BUILDRPMS_SCRIPT}" rpm
 
 # Building TopoLVM upstream RPM
 COPY --chown=${USER}:${USER} ./src/topolvm/topolvm.spec "${HOME}/microshift/packaging/rpm/microshift.spec"
@@ -71,7 +73,7 @@ COPY --chown=${USER}:${USER} ./src/topolvm/assets/  "${HOME}/microshift/assets/o
 COPY --chown=${USER}:${USER} ./src/topolvm/dropins/ "${HOME}/microshift/packaging/microshift/dropins/"
 COPY --chown=${USER}:${USER} ./src/topolvm/greenboot/ "${HOME}/microshift/packaging/greenboot/"
 COPY --chown=${USER}:${USER} ./src/topolvm/release/ "${HOME}/microshift/assets/optional/topolvm/"
-RUN MICROSHIFT_VARIANT="community" make -C "${HOME}/microshift" rpm
+RUN "${USHIFT_BUILDRPMS_SCRIPT}" rpm
 
 # Post-build MicroShift configuration
 COPY --chmod=755 ./src/image/postbuild.sh ${USHIFT_POSTBUILD_SCRIPT}
---
+++ src/image/build-rpms.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+# Primary purpose of this script is to build the MicroShift RPMs and SRPMs
+# with adjusted version string.
+# Using `make {rpm,srpm}` it would hardcode a meaningless version string based on downstream git variables
+# which would not provide good information to identify the build and trace back its contents.
+# Following script overrides the version to include information about the downstream version and commit, and OKD tag.
+
+target=all
+if [[ "$#" -eq 1 ]]; then
+    if [[ "${1}" != "all" && "${1}" != "rpm" && "${1}" != "srpm" ]]; then
+        echo "Script accepts at most one argument: all, rpm or srpm"
+        echo "If no argument is provided, the default is 'all'"
+        exit 1
+    fi
+    target="${1}"
+fi
+
+cd "${HOME}/microshift"
+
+SOURCE_GIT_COMMIT="$(git rev-parse --short 'HEAD^{commit}')"
+
+# MICROSHIFT_VERSION must start with X.Y.Z for the internals to correctly parse the version.
+# If USHIFT_REF is a tag, use it. Otherwise parse the version from Makefile.version.*.var file.
+if [[ $(git tag -l "${USHIFT_REF}") ]]; then
+    MICROSHIFT_VERSION="${USHIFT_REF}"
+else
+    MICROSHIFT_VERSION="$(awk -F'[=.-]' '{print $2 "." $3 "." $4}' Makefile.version.aarch64.var | sed -e 's/ //g')"
+fi
+# Example results:
+# - 4.21.0_ga9cd00b34_4.21.0_okd_scos.ec.5                for build against HEAD of main which was 4.21 at the time.
+# - 4.20.0-202510201126.p0-g1c4675ace_4.20.0-okd-scos.6   for build against a specific tag.
+MICROSHIFT_VERSION="${MICROSHIFT_VERSION}-g${SOURCE_GIT_COMMIT}-${OKD_VERSION_TAG}"
+# MicroShift's make-rpm.sh makes this substitution. Although we don't use the script,
+# let's do it as well for keeping the version consistent with existing downstream RPMs.
+# Version is also used for release.md file.
+MICROSHIFT_VERSION=${MICROSHIFT_VERSION//-/_}
+
+RPM_RELEASE="1"
+SOURCE_GIT_TAG="$(git describe --long --tags --abbrev=7 --match 'v[0-9]*' || echo "v0.0.0-unknown-${SOURCE_GIT_COMMIT}")"
+SOURCE_GIT_TREE_STATE=clean # Because we're updating downstream specfile, but that shouldn't be a reason to have -dirty suffix.
+MICROSHIFT_VARIANT=community
+
+export MICROSHIFT_VERSION
+export RPM_RELEASE
+export SOURCE_GIT_TAG
+export SOURCE_GIT_COMMIT
+export SOURCE_GIT_TREE_STATE
+export MICROSHIFT_VARIANT
+
+if [[ "${target}" == "all" || "${target}" == "rpm" ]]; then
+    ./packaging/rpm/make-rpm.sh rpm local
+fi
+
+if [[ "${target}" == "all" || "${target}" == "srpm" ]]; then
+    ./packaging/rpm/make-rpm.sh srpm local
+fi
+
+echo "${MICROSHIFT_VERSION}" > "${HOME}/microshift/_output/rpmbuild/RPMS/version.txt"
