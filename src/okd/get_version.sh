ed4ced12598a3eceea63e87d80790c7e4a1f8e80
Gregory Giguashvili <ggiguash@redhat.com>
2025-12-09 18:25:00 +0200

    Simplify the get_version script by querying release images on both platforms

--- src/okd/get_version.sh
+++ src/okd/get_version.sh
@@ -1,45 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-QUERY_URL_AMD64=${QUERY_URL_AMD64:-quay.io/api/v1/repository/okd/scos-release}
+QUERY_URL_AMD64=${QUERY_URL_AMD64:-quay.io/okd}
 QUERY_URL_ARM64=${QUERY_URL_ARM64:-ghcr.io/microshift-io/okd}
 
 function usage() {
-    echo "Usage: $(basename "$0") <latest | latest-amd64 | latest-arm64>" >&2
+    echo "Usage: $(basename "$0") <latest-amd64 | latest-arm64>" >&2
     echo "" >&2
-    echo "Get the latest OKD version tag based on the specified 'latest'," >&2
-    echo "'latest-amd64', or 'latest-arm64' command line argument." >&2
+    echo "Get the latest OKD version tag based on the specified 'latest-amd64'" >&2
+    echo "or 'latest-arm64' command line argument." >&2
     exit 1
 }
 
 function get_amd64_version_tags() {
     local -r query_file="$1"
 
-    local npage=1
-    local more_pages=true
-
-    while [ "${more_pages}" = "true" ]; do
-        local query_url="https://${QUERY_URL_AMD64}/tag/?limit=100&page=${npage}"
-        local query_response
-
-        if ! query_response="$(curl -s --max-time 60 "${query_url}")" ; then
-            echo "ERROR: Failed to query the OKD version tags from '${query_url}'" >&2
-            exit 1
-        fi
-
-        # Save the current page content to the query file
-        if ! echo "${query_response}" | jq -r ".tags[].name" >> "${query_file}" ; then
-            echo "ERROR: Failed to save the current page content to '${query_file}'" >&2
-            exit 1
-        fi
-        # Check if there are more pages to query
-        if ! more_pages="$(echo "${query_response}" | jq -r '.has_additional')" ; then
-            echo "ERROR: Failed to check if there are more pages to query from '${query_url}'" >&2
-            exit 1
-        fi
-        # Increment the page number
-        npage=$(( npage + 1 ))
-    done
+    if ! skopeo list-tags "docker://${QUERY_URL_AMD64}/scos-release" | jq -r .Tags[] >> "${query_file}" ; then
+        echo "ERROR: Failed to get the OKD version tags from '${QUERY_URL_AMD64}/scos-release'" >&2
+        exit 1
+    fi
 }
 
 function get_arm64_version_tags() {
@@ -65,10 +44,6 @@ trap 'rm -f "${query_file}" "${version_file}"' EXIT
 
 # Read all version tags from the repositories
 case "$1" in
-    latest)
-        get_amd64_version_tags "${query_file}"
-        get_arm64_version_tags "${query_file}"
-        ;;
     latest-amd64)
         get_amd64_version_tags "${query_file}"
         ;;
